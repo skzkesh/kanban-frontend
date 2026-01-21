6df41cb5a96db2ee3b233910f75d50474eeda3a1
Keshia Poedjiono <keshiapud@gmail.com>
2026-01-21 15:57:36 +0800

    configure Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Kanban App
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
