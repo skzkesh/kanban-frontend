4cc3be5f13d9d475950709cb4a79baf7a89e59ca
Keshia Poedjiono <keshiapud@gmail.com>
2026-01-26 16:35:13 +0800

    modify card container for signup

--- src/components/Signup/CardContainer.tsx
+++ src/components/Signup/CardContainer.tsx
@@ -6,6 +6,6 @@ type CardContainerProps = {
 
 export function CardContainer({children}: CardContainerProps){
     return (
-        <div className="border border-gray-300 w-full max-w-sm mx-auto h-96 p-4 justify-center">{children}</div>
+        <div className="border shadow-md rounded-lg w-full max-w-sm mx-auto h-96 p-4 justify-center">{children}</div>
     );
 }
